510890220d3447a21a993ac9abaadf6b4cae4a74
Ushank Radadiya <ushank@getlokalapp.com>
2024-04-06 15:08:25 +0530

    initial commit

--- helpdesk/settings.py
+++ helpdesk/settings.py
@@ -160,3 +160,34 @@ AUTH_USER_MODEL = "user_accounts.Account"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # set this
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
+
+#
+# CSRF_TRUSTED_ORIGINS = [
+#     'http://localhost:3000',
+# ]
+#
+# CORS_ALLOWED_ORIGINS = [
+#     'http://localhost:3000',
+#     'http://localhost:8000',
+#     'http://127.0.0.1:3000',
+#     'http://127.0.0.1:8000',
+# ]
